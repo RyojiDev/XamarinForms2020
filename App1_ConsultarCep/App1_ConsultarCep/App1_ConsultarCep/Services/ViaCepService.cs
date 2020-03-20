eb72c239aa53405210cf99f93f3b7e25bb16d734
Ryojidev <ryojikitanodev@gmail.com>
2020-03-20 11:37:40 -0300

    init

---
+++ App1_ConsultarCep/App1_ConsultarCep/App1_ConsultarCep/Services/ViaCepService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1_ConsultarCep.Services.Model;
+using Newtonsoft.Json;
+
+
+namespace App1_ConsultarCep.Services
+{
+    public class ViaCepService
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
